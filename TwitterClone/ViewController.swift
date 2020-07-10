6d7765160893ee98eaffc8690307156123f809ba
Andrey Novikov <andrenovikov2000@mail.ru>
2020-07-10 10:08:09 +0600

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Andrey Novikov on 7/10/20.
+//  Copyright © 2020 Andrey Novikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
